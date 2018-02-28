aefb47790f94f4d65392a25d245d77951064cabe
futao <futao@bonc.com.cn>
2018-02-28 11:01:29 +0800

    默认集合初始化

--- collector/src/main/java/com/navercorp/pinpoint/collector/dao/es/ESTraceDaoV2.java
+++ collector/src/main/java/com/navercorp/pinpoint/collector/dao/es/ESTraceDaoV2.java
@@ -1,6 +1,5 @@
 package com.navercorp.pinpoint.collector.dao.es;
 
-
 import org.springframework.stereotype.Repository;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -19,18 +18,17 @@ public class ESTraceDaoV2 implements TraceDao {
 	public void insert(final SpanBo spanBo) {
 		// TODO Auto-generated method stub
 		if (spanBo == null) {
-            throw new NullPointerException("spanBo must not be null");
-        }
+			throw new NullPointerException("spanBo must not be null");
+		}
 
-
-        TransactionId transactionId = spanBo.getTransactionId();
-        String agentId = transactionId.getAgentId();
-        String id = agentId + EsIndexs.ID_SEP + transactionId.getAgentStartTime() + EsIndexs.ID_SEP + transactionId.getTransactionSequence();
-        try {
+		TransactionId transactionId = spanBo.getTransactionId();
+		String agentId = transactionId.getAgentId();
+		String id = agentId + EsIndexs.ID_SEP + transactionId.getAgentStartTime() + EsIndexs.ID_SEP
+				+ transactionId.getTransactionSequence();
+		try {
 			ObjectMapper mapper = new ObjectMapper();
 			byte[] json = mapper.writeValueAsBytes(spanBo);
-			EsClient.client().prepareIndex(EsIndexs.TRACE_V2, EsIndexs.TYPE, id)
-					.setSource(json).get();
+			EsClient.client().prepareIndex(EsIndexs.TRACE_V2, EsIndexs.TYPE, id).setSource(json).get();
 		} catch (JsonProcessingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -41,18 +39,19 @@ public class ESTraceDaoV2 implements TraceDao {
 	public void insertSpanChunk(SpanChunkBo spanChunkBo) {
 		// TODO Auto-generated method stub
 		TransactionId transactionId = spanChunkBo.getTransactionId();
-        
-		 String id = transactionId.getAgentId();
-	        
-	        try {
-				ObjectMapper mapper = new ObjectMapper();
-				byte[] json = mapper.writeValueAsBytes(spanChunkBo);
-				EsClient.client().prepareIndex(EsIndexs.TRACE_V2, EsIndexs.TYPE, id)
-						.setSource(json).get();
-			} catch (JsonProcessingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+
+		String agentId = transactionId.getAgentId();
+		String id = agentId + EsIndexs.ID_SEP + transactionId.getAgentStartTime() + EsIndexs.ID_SEP
+				+ transactionId.getTransactionSequence();
+
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			byte[] json = mapper.writeValueAsBytes(spanChunkBo);
+			EsClient.client().prepareIndex(EsIndexs.TRACE_V2, EsIndexs.TYPE, id).setSource(json).get();
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
