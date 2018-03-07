9056e2b1116649f7d8226b225bbe81079b686847
futao <futao@bonc.com.cn>
2018-03-07 16:26:16 +0800

    修改入库es时区

--- collector/src/main/java/com/navercorp/pinpoint/collector/util/BeanToJson.java
+++ collector/src/main/java/com/navercorp/pinpoint/collector/util/BeanToJson.java
@@ -1,25 +1,36 @@
 package com.navercorp.pinpoint.collector.util;
 
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
+
 import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class BeanToJson {
+	
+	public static SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 
 	public static <T> JSONObject toEsTime(T t) throws JsonProcessingException {
 		ObjectMapper mapper = new ObjectMapper();
 		String text = mapper.writeValueAsString(t);
 		JSONObject jsonbject = JSONObject.parseObject(text);
-		jsonbject.put("@timestamp", Long.toString(System.currentTimeMillis()));
+		Calendar calendar = Calendar.getInstance();
+		df.setTimeZone(TimeZone.getTimeZone("UTC"));
+		String timestamp = df.format(calendar.getTime());
+		jsonbject.put("@timestamp", timestamp);
 		return jsonbject;
 	}
 	
 	public static JSONObject addEsTime(JSONObject jsonbject) {
 		if(jsonbject != null) {
-			jsonbject.put("@timestamp", Long.toString(System.currentTimeMillis()));
+			Calendar calendar = Calendar.getInstance();
+			df.setTimeZone(TimeZone.getTimeZone("UTC"));
+			String timestamp = df.format(calendar.getTime());
+			jsonbject.put("@timestamp", timestamp);
 		}
 		return jsonbject;
 	}
-	
 }
